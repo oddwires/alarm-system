b776182723b9705fb443d123827e2fdc1523ac3b
Tony <tony@oddwires.co.uk>
2017-01-21 17:32:15 +0000

    Full screen fix
    Hasn't 100% fixed it - but its a lot better.
    Immediate reload still fails - but reloading after 10 seconds seems ok.

--- dologin.php
+++ dologin.php
@@ -1,4 +1,9 @@
 <?php
+// Date in the past
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+
 $loginisvalid=FALSE;
 $username=$_POST['username'];
 $password_entered=$_POST['password'];
