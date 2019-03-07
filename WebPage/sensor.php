ed678fd2490510c6958f8083d59f10de623de690
Tony <tony@oddwires.co.uk>
2019-03-07 20:07:31 +0000

    Prevent access from external IP's

--- WebPage/sensor.php
+++ WebPage/sensor.php
@@ -1,3 +1,12 @@
+<?php
+    if (strpos($_SERVER['REMOTE_ADDR'], '192.168.1') === false) {
+        // Block access from external IP's
+        header('HTTP/1.0 403 Forbidden');
+        echo 'Access to this web page is forbidden!';
+        exit;
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
@@ -13,7 +22,15 @@
     $HKDataFile = "accessory.txt";                                        // latest values allowing fast access by HK accessories.
     $NewHKdata = array();                                                 // used to collate new values contained in post
 
-    if($Debug) { error_log(print_r($_POST, true)); }
+    if($Debug) { 
+        if (strpos($_SERVER['REMOTE_ADDR'], '192.168.1') !== false) {
+            echo 'local IP address. Access permitted.<br>';
+        }
+        echo "Remote IP...<br>";
+        var_dump($_SERVER['REMOTE_ADDR']);
+        echo "<br>POST data...<br>";
+        var_dump(print_r($_POST, true));
+    }
 
     if (file_exists($Path.$FlotDataFile)) {
         // if the file exists, read the contents..
