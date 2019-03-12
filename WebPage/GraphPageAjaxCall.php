448db23161ef549bf74a588ca43fb292aa2caf92
Tony <tony@oddwires.co.uk>
2019-03-12 20:00:06 +0000

    Fix temperature plots ( sort of )

--- WebPage/GraphPageAjaxCall.php
+++ WebPage/GraphPageAjaxCall.php
@@ -2,28 +2,46 @@
 <body>
     <script type="text/javascript">
     <?php
-    // Code to scan for any number of temperature data files, and merge them into a single
-    // JSON file that can be used with FLOT.
-        $date = date('d\-m\-y');
-        $filter = "/var/www/logs/Temperature-*".$date."*.csv";
-        $list = glob($filter);
-        $JSON_string = "{\n";
-        foreach($list as $fileName) {                                       // loop through all files found with today's date...
-            $roomName = explode("-",$fileName);                             // create array
-            $roomName = $roomName[1];                                       // get just the room name
-            $JSON_string .= "    \"" . $roomName . "\": {\n";
-            $JSON_string .= "        label: \"" . $roomName . "\",\n";
-            $JSON_string .= "        data: ";
-            $content = file_get_contents($fileName);                         // Now read the raw ESP8266 data file
-            $content = str_replace("],[", ",", $content)."]";                // NEED TO CHANGE FORMAT IN ESP8266 CODE !
-            $content = "[".str_replace("\n", ", ", $content)."]";
-            $content = str_replace(", ]]", "]", $content);
-            $JSON_string .= $content;
-            $JSON_string .= "\n    },\n";
-        }
-        $JSON_string .= "}\n";
-    // $file = '/var/www/logs/outfile.txt';                                  // DEBUG - write JSON object to text file
-    // file_put_contents($file, $JSON_string);
+/* Code to scan for any number of sensor data files, and merge them into a single JSON file that can be used with FLOT.
+
+   FLOT requires data to be a JSON object with the data element being an array. Example:-
+    $JSON_string =
+    '{
+        "USA":{
+            "label":"USA",
+            "data":[[1988, 483994], [1989, 479060], [1990, 457648], [1991, 401949], [1992, 424705], [1993, 402375], [1994, 377867], [1995, 357382], [1996, 337946], [1997, 336185], [1998, 328611], [1999, 329421], [2000, 342172], [2001, 344932], [2002, 387303], [2003, 440813], [2004, 480451], [2005, 504638], [2006, 528692]]
+            },
+        "Russia":{
+             "label":"Russia",
+            "data":[[1988, 218000], [1989, 203000], [1990, 171000], [1992, 42500], [1993, 37600], [1994, 36600], [1995, 21700], [1996, 19200], [1997, 21300], [1998, 13600], [1999, 14000], [2000, 19100], [2001, 21300], [2002, 23600], [2003, 25100], [2004, 26100], [2005, 31100], [2006, 34700]]
+            }
+    }'
+*/
+    $Path = '/var/data/app-sensor/';
+    $date = date('d\-m\-y');
+    $filter = $Path."*.csv";
+    $list = glob($filter);
+
+    $JSON_string = "{\r\n";
+    foreach($list as $fileName) {                                         // loop through all files found in data directory...
+        $label = str_replace($Path, "", $fileName);                       // strip out the path
+        $label = str_replace(".csv", "", $label);                         // strip off file extension
+
+        $JSON_string .= '     "'.$label.'"'.":{\r\n";
+        $JSON_string .= '         "label":"'.$label.'"'.",\r\n";
+        $JSON_string .= '         "data":[';
+
+        $content = file_get_contents($fileName);                          // Now read the raw ESP8266 data file
+        $content = str_replace("\n", "", $content);                       // remove all the new lines
+        $content = str_replace("],", "], ", $content);                    // improve readability
+        $content = substr($content, 0, -2);                               // remove the last comma
+        $content .= "]";                                                  // terminate the data section
+
+        $JSON_string .= $content;
+        $JSON_string .= "\r\n     },\n";
+    }
+    $JSON_string = substr($JSON_string, 0, -2);                           // remove the last comma
+    $JSON_string .= "\r\n}\n";
     ?>
     var datasets = <?php echo $JSON_string; ?>
     </script>
@@ -34,7 +52,7 @@
                 <span class="gloss"></span>
                 <span class="SquareButtonText">Menu</span>
             </a>
-            <h1 style="font-size: 22px; padding: .35em 0;"><?php echo $RCnum; ?>Temperatures for: <?php echo $date ?></h1>
+            <h1 style="font-size: 22px; padding: .35em 0;">Temperatures for: <?php echo $date ?></h1>
         </div>
 
         <div id="chart" style="width: 100%; height: 300px; float:left; background-color: #aac39de8; margin-top: 50px;"></div>
--- WebPage/sensor.php
+++ WebPage/sensor.php
@@ -16,10 +16,8 @@
 
     $Debug = false;                                                       // used to dump additional data for debugging
     $Timestamp = time() * 1000;                                           // Unix timestamp converted to Javascript
-    $DateStamp = date('d-m-y');
-    $Path = '/var/www/logs/';
-    $FlotDataFile = "flot.txt";                                           // historic data in JSON format.
-    $HKDataFile = "accessory.txt";                                        // latest values allowing fast access by HK accessories.
+    $Path = '/var/data/app-sensor/';
+    $HKDataFile = "current_values.txt";                                   // just the latest values - allows fast access by HK accessories.
     $NewHKdata = array();                                                 // used to collate new values contained in post
 
     if($Debug) { 
@@ -32,68 +30,59 @@
         var_dump(print_r($_POST, true));
     }
 
-    if (file_exists($Path.$FlotDataFile)) {
-        // if the file exists, read the contents..
-        $myfile = fopen($Path.$FlotDataFile, "r+");
-        $serialized = fread($myfile,filesize($Path.$FlotDataFile));
-        fclose($myfile);
-    } else {
-        // if the file doesn't exist, initialise the variable cleanly...
-        $serialised = "";                                                  
+    if($Debug) {
+        echo "<br>Timestamp: ".$Timestamp."<br>";
     }
-    $jsonOBJ = json_decode($serialized,true);                             // convert to associative array
 
-    if ($Debug) {
-        echo "<br>=============================<br>";
-        var_dump($jsonOBJ);
-    }
-   
-   // Loop through the input data array
+// Loop through the input data array
    $count = 0;
    foreach ($_POST['sensor'] as $thisSensor) {
         // Do something with each valid entry ...
         if ($thisSensor != "") {
             $thisValue = $_POST['temperature'][$count];
-            $jsonOBJ[$thisSensor][$Timestamp] = $thisValue;               // add new data to Flot data object
+
+            // Record the data to log file for graphs...
+            $FileName = $thisSensor.'.csv';
+            $data = "[".$Timestamp.", ".$thisValue."],";
+            $myfile = file_put_contents($Path.$FileName, $data.PHP_EOL , FILE_APPEND | LOCK_EX);
+
+            // Record the data to log file for HomeKit...
             $NewHKdata[$thisSensor] = $thisValue;                         // add data to KomeKit data array
         }
         $count ++;
     }
-    if ($Debug) {
-        echo "<br>=============================<br>";
-        echo "<br>Edited Flot data array...<br>";
-        var_dump($jsonOBJ);
 
-        echo "<br>=============================<br>";
-        echo "<br>New HK data array...<br>";
-        var_dump($NewHKdata);
-    }
-
-    // Note: File will only fail to open if the folder permissions are wrong.
-    $myfile = fopen($Path.$FlotDataFile, "w+") or die("Unable to open file: $Path$FlotDataFile");
-    $serialized = json_encode($jsonOBJ);
-    fwrite($myfile, $serialized);                                         // save it
-    fclose($myfile);                                                      // close it
-
-   // Record data to second file for HomeKit accessories...
+   // Write data to second file for HomeKit accessories...
    if(file_exists($Path.$HKDataFile)){
         $serialized = file_get_contents($Path.$HKDataFile);
         $temperatures = json_decode($serialized,true);                    // convert to associative array
     } else {
-          $temperatures="";
+          $temperatures = array();
     }
-    $temperatures = array_replace($temperatures,$NewHKdata);              // merge new data
-    $serialized = json_encode($temperatures);                             // convert to JSON format
-    file_put_contents($Path.$HKDataFile, $serialized);
 
     if ($Debug) {
         echo "<br>=============================<br>";
         echo "<br>Data read from HK file...<br>";
         var_dump($temperatures);
         echo "<br>=============================<br>";
-        echo "<br>Data to be written to HK file...<br>";
+        echo "<br>Data to be merged into existing HK file...<br>";
+        var_dump($NewHKdata);
+        echo "<br>=============================<br><br>";
+    }
+
+    $temperatures = array_replace($temperatures,$NewHKdata);              // merge new data
+
+    if ($Debug) {
+        echo "<br>=============================<br>";
+        echo "<br>Data following array merge...<br>";
         var_dump($temperatures);
         echo "<br>=============================<br><br>";
+    }
+
+    $serialized = json_encode($temperatures);                             // convert to JSON format
+    file_put_contents($Path.$HKDataFile, $serialized);
+
+    if ($Debug) {
         echo "JSON encoded data...<br>";
         var_dump($serialized);
         echo "<br>=============================<br><br>";
